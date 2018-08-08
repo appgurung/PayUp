ef48ec425b4c9b0c4ab7542b953ec796fb6ad0e2
appgurung <laolex.stephen@gmail.com>
2018-08-08 11:02:32 +0100

    CashBox

--- CashBox/CashBox/CashBox/CashBox.Designer.cs
+++ CashBox/CashBox/CashBox/CashBox.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Tivas Technologies\Documents\Visual Studio 2017\Projects\CashBox\CashBox\CashBox.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\SwiftBox\Git\PayUp\CashBox\CashBox\CashBox\CashBox.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
